c1a0bdd71fb40ea24ca94d8046614b0ad79a77f6
pimshanokn <110382820+pimshanokn@users.noreply.github.com>
2022-08-11 13:04:49 +0700

    Add files via upload

---
+++ pm4_page4.js
@@ -0,0 +1,54 @@
+/*  FILE NAME: pm4_page4.html
+    WRITTEN BY: Moji Niyamanusorn
+    DATE: December 21, 2021
+    Purpose: CS115/ΜΑΣ115 project PM5
+*/
+
+"use strict";
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+// start of slideshow
+// tutorial on how to embed slideshow from W3Schools (https://www.w3schools.com/howto/howto_js_slideshow.asp)
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
